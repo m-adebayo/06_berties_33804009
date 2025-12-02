9c6938e8975bcf6e4d163dcf8cdbada2c693934e
m-adebayo <malachi.adebayo0@gmail.com>
2025-12-02 11:29:09 +0000

    Providing API, Updated README

---
+++ routes/api.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/books', (req, res, next) => {
+    let search = req.query.search;
+    let minPrice = req.query.minprice;
+    let maxPrice = req.query.maxprice;
+
+    let sqlquery = "SELECT * FROM books";
+    let conditions = [];
+    let params = [];
+   
+    if (search) {
+        conditions.push("book_name LIKE ?");
+        params.push(`%${search}%`);
+    }
+
+
+    if (minPrice) {
+        conditions.push("price >= ?");
+        params.push(minPrice);
+    }
+
+    if (maxPrice) {
+        conditions.push("price <= ?");
+        params.push(maxPrice);
+    }
+
+    if (conditions.length > 0) {
+        sqlquery += " WHERE " + conditions.join(" AND ");
+    }
+
+    db.query(sqlquery, params, (err, result) => {
+        if (err) {
+            res.json({ error: err });
+            return next(err);
+        }
+        res.json(result);
+    });
+});
+
+module.exports = router;
